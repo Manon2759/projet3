553da562c3aa66bbc77781898f9cbac32de58235
vincent <vincentcharlettepro@gmail.com>
2022-04-21 12:13:00 +0200

    liaison front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-
+import { io } from "socket.io-client";
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const socket = io('http://localhost:5000')
+    socket.on("connect", () => {
+      console.log("connected");
+    })
+
+  }, [])
   return (
     <div className="App">
 
+      <p>test</p>
     </div>
   );
 }
