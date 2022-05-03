275a37c3cf907ebff133d067f391be590a29571c
vincent <vincentcharlettepro@gmail.com>
2022-05-03 16:29:02 +0200

    inscription premiere page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 import React, { useEffect } from "react";
 import Header from "./components/Header";
+import Inscritpion from "./components/Inscription";
+import Footer from "./components/Footer";
 
 
 const App = () => {
@@ -15,6 +17,8 @@ const App = () => {
     <div className="App">
 
       <Header />
+      <Inscritpion />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Inscription.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Inscription = () => {
+    return (
+        <div className='inscription'>
+            <h1>INSCRIPTION</h1>
+            <input type="text" id="pseudo" name="Pseudo" />
+
+
+
+            <button className='inscription_button'>Validation</button>
+
+        </div>
+    );
+};
+
+export default Inscription;
