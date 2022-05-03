da2c5fed9e421f194fc2da9a5c74e9afcbf4aafe
Manon VAVASSEUR <Manon.vavasseur@live.fr>
2022-05-03 15:15:20 +0200

    maj

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+
 
 const Footer = () => {
     return (
         <div>
+            <div className='footer'>
 
+                <img src='/assets/logo-trainder.png' alt='logo-trainder' />
+
+                <div className='links_footer'>
+                    <NavLink className='link' to='/'> Contact <span>|</span> </NavLink>
+                    <NavLink className='link' to='' > Réglementation  <span>|</span></NavLink >
+                </div>
+                
+                <a href='https://www.facebook.com/search/top?q=spotted%20sncf'><FaFacebook /></a>
+                <a href='https://twitter.com/SNCF'><FaTwitter /></a>
+                <a href='https://www.instagram.com/sncf/'><FaInstagram /></a>
+
+            </div>
         </div>
     );
 };
