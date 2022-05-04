ac93a1ce8a6dea945e5c2bf288be5e74d07a8b0e
vincent <vincentcharlettepro@gmail.com>
2022-05-04 14:15:36 +0200

    first step inscription

--- frontend/src/components/Inscription.jsx
+++ frontend/src/components/Inscription.jsx
@@ -2,15 +2,33 @@ import React from 'react';
 
 const Inscription = () => {
     return (
-        <div className='inscription'>
-            <h1>INSCRIPTION</h1>
-            <input type="text" id="pseudo" name="Pseudo" />
+
+        <div className="inscription">
+
+            <div className="inscription_titre">
+                <h1>INSCRIPTION</h1>
+            </div>
+            <div className="inscription_main">
+                <div className="inscription_pseudo">
+                    <label for="pseudo" id="label_pseudo">Pseudo  </label>
+                    <input type="text" id="pseudo" name="Pseudo" />
+                </div>
 
 
+                <div className="inscription_mail">
+                    <label for="mail" id="label_mail">Mail  </label>
+                    <input type="email" id="mail" name="Mail" />
+                </div>
 
-            <button className='inscription_button'>Validation</button>
+                <div className="inscription_password">
+                    <label for="password" id="label_password">Mot de passe  </label>
+                    <input type="text" id="password" name="Password" />
+                </div>
 
-        </div>
+                <div className="inscription_button"><button>Validez</button></div>
+            </div>
+        </div >
+
     );
 };
 
