e4af5821f3fe2e7e6b92974c6be0d85de8f52f70
Leila <leilaaa77220@gmail.com>
2022-05-05 09:28:56 +0200

    Card profil finale

--- frontend/src/components/Passport.jsx
+++ frontend/src/components/Passport.jsx
@@ -18,27 +18,35 @@ const Passport = () => {
                     <div className='card'>
                         <div className='card-image'>
 
-                            <img src='/assets/fille3.jpg' alt="profile one" onMouseOver={changeTrue} onMouseLeave ={changeFalse} />
+                            <img src='/assets/fille3.jpg' alt="profile one" onMouseOver={changeTrue} onMouseLeave={changeFalse} />
 
                         </div>
                         {description &&
-                        <div className='card_description'>
-                        <h1>Description :</h1>
-                        <p>Salut, je suis passionné de voyages, de sport et d'informatique.N'hésitez pas à me contacter si tu as envie de papoter 🥰</p>
-                        
-                        <ul className='social-icons'>
-                            <li>
-                                <a href=""><FcLike />
-                                    <i class="fab fa-heart-f"></i>
-                                </a>
-                            </li>
-                        </ul>
-                        </div>
-}
+                            <div className='card_description'>
+                                <h1>Description :</h1>
+                                <p>Salut, je suis passionnée de voyages, de sport et d'informatique.N'hésitez pas à me contacter si tu as envie de papoter 🥰</p>
+                                <h1>Centres d'intérêt :</h1>
+                                <div className='list'>
+                                    <ul>
+                                        <li>Voyages 🌍</li>
+                                        <li>Nouvelles technologies 💻</li>
+                                        <li>Sport 🏆</li>
+                                        <li>Lecture 📚</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        }
                         <div className='details'>
                             <div className='name'>
                                 <h1>Maria Lopez</h1>
                             </div>
+                            <ul className='social-icons'>
+                                <li>
+                                    <a href=""><FcLike />
+                                        <i class="fab fa-heart-f"></i>
+                                    </a>
+                                </li>
+                            </ul>
                             <div className='info'>
                                 <h3> 🎂30 ans</h3>
                                 <h3> 📍Paris</h3>
